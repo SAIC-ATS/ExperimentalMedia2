e1215ace47f9ebbba2e18fc446a3bd59d49a7ce1
kmcwharter <kristinmcwharter@gmail.com>
2022-10-11 09:29:53 -0500

    Update sketch.js

--- download/sketch.js
+++ download/sketch.js
@@ -1,9 +1,10 @@
 let txt = "";
 let btn = "SAVE ME!";
 const assignment = JSON.parse(localStorage.getItem("prompts"));
+let x, y;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   let button = createButton(btn);
   button.position(width / 2 - 50, height / 2 + 50);
   button.mousePressed(saveMe);
@@ -11,7 +12,9 @@ function setup() {
 }
 
 function draw() {
-  background(255, 200, 100);
+  x = map(mouseX, 0, width, 50, 200);
+  y = map(mouseY, 0, height, 100, 20);
+  background(255, x, y);
   text(txt, width / 4, height / 3, width / 2);
 }
 
